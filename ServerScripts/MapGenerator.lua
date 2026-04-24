124bd9c9aeec732c902ca0be3486783a7596ed7f
Cho Huiyeon <jhy01108@g.skku.edu>
2026-04-24 21:49:30 +0900

    Add map generation script

---
+++ ServerScripts/MapGenerator.lua
@@ -0,0 +1,9 @@
+-- Map Generation Script
+-- This script generates a random map for the Roblox Rocket Game.
+
+local function generateMap()
+    print("Generating map...")
+    -- Add your map generation logic here
+end
+
+generateMap()
